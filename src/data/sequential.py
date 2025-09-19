191708ad2a91a409b397263fb194f518b1e13b11
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-19 23:49:32 +0100

    Update src/data/sequential.py: In Progress

--- src/data/sequential.py
+++ src/data/sequential.py
@@ -1,5 +1,21 @@
 
+import src.data.api
+import src.elements.partitions as pr
+
 class Sequential:
 
-    def __init__(self):
+    def __init__(self, starting: str, ending: str, partitions: pr.Partitions):
+
+        self.__starting = starting
+        self.__ending = ending
+
+        self.__data: list[dict] = src.data.api.API().exc(
+            starting=self.__starting, ending=self.__ending)
+
+        self.__codes = list(map(lambda x: x.ts_id, partitions))
+
+    def __get_values(self):
+        pass
+
+    def exc(self):
         pass
