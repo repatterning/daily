e4a7d191b4e4d814bfca73bc6c59f0930786eee7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-20 17:32:16 +0100

    Add src/data/filtering.py

---
+++ src/data/filtering.py
@@ -0,0 +1,45 @@
+import logging
+import sys
+import pandas as pd
+
+import src.functions.cache
+
+class Filtering:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def __filter(gauges: pd.DataFrame, attributes: dict) -> pd.DataFrame:
+        """
+
+        :param gauges:
+        :return:
+        """
+
+        codes: list = attributes.get('excerpt')
+
+        # Feed
+        catchments = gauges.copy().loc[gauges['ts_id'].isin(codes), 'catchment_id'].unique()
+        if sum(catchments) == 0:
+            src.functions.cache.Cache().exc()
+            sys.exit('None of the time series codes is valid')
+
+        _gauges = gauges.copy().loc[gauges['catchment_id'].isin(catchments), :]
+
+        # Logging
+        elements = _gauges['ts_id'].unique()
+        logging.info('The feed is requesting emergency intelligence for %s gauges, %s.  '
+                     'Intelligence is possible for %s gauges, %s', len(codes), codes, elements.shape[0], elements)
+
+        return _gauges
+
+    def exc(self, gauges: pd.DataFrame, attributes: dict) -> pd.DataFrame:
+        """
+
+        :param gauges:
+        :param attributes:
+        :return:
+        """
+
+        return self.__filter(gauges=gauges, attributes=attributes)
