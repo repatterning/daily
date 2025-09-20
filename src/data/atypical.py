47beb7dd9f3e2af723287191f9208d0f22538651
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-20 22:48:41 +0100

    Update src/data/atypical.py

--- src/data/atypical.py
+++ src/data/atypical.py
@@ -1,5 +1,6 @@
-
+"""Module atypical.py"""
 import datetime
+import boto3
 
 import src.data.api
 import src.data.sequential
@@ -11,9 +12,11 @@ class Atypical:
     Addresses the straddling of years
     """
 
-    def __init__(self, s3_parameters: s3p.S3Parameters, codes: dict):
+    def __init__(self, connector: boto3.session.Session, s3_parameters: s3p.S3Parameters, codes: dict):
         """
 
+        :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
+                          Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
         :param s3_parameters:
         :param codes:
         """
@@ -22,7 +25,7 @@ class Atypical:
         self.__codes = codes
 
         # An instance for reading gauge data
-        self.__api = src.data.api.API()
+        self.__api = src.data.api.API(connector=connector)
 
     def continuous(self, settings: dict):
         """
