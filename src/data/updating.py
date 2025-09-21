25e008bd821ce6d8a8a9266d6f10be8e57eb9a49
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-21 01:15:47 +0100

    Update src/data/updating.py

--- src/data/updating.py
+++ src/data/updating.py
@@ -1,9 +1,12 @@
 """Module data/updating.py"""
 import pandas as pd
+import os
 
+import config
 import src.elements.s3_parameters as s3p
 import src.elements.text_attributes as txa
 import src.functions.streams
+import src.functions.directories
 
 
 class Updating:
@@ -23,25 +26,29 @@ class Updating:
         self.__settings = settings
 
         # Instances
+        self.__configurations = config.Config()
+        self.__directories = src.functions.directories.Directories()
         self.__streams = src.functions.streams.Streams()
 
-    def __update(self, uri: str, frame: pd.DataFrame) -> str:
+    def __update(self, uri: str, frame: pd.DataFrame, affix: str) -> str:
         """
 
         :param uri: Storage path.
         :param frame: The data of a gauge.
+        :param affix:
         :return:
         """
 
         # If a file of interest does not exist, an empty data frame is returned
         text = txa.TextAttributes(uri=uri, header=0)
         original = self.__streams.read(text=text)
-
-        # Concatenating
         instances = pd.concat([original, frame], axis=0, ignore_index=True)
         instances.drop_duplicates(inplace=True)
 
-        return self.__streams.write(blob=instances, path=uri)
+        path = os.path.join(self.__configurations.series_, affix)
+        self.__directories.create(path=os.path.dirname(path))
+
+        return self.__streams.write(blob=instances, path=path)
 
     def exc(self, frame: pd.DataFrame, ts_id: int, catchment_id: int) -> str:
         """
@@ -53,7 +60,7 @@ class Updating:
         """
 
         date = self.__settings.get('year') + '-01-01'
-        uri = (f's3://{self.__s3_parameters.internal}/{self.__s3_parameters.path_internal_data}series/'
-               f'{catchment_id}/{ts_id}/{date}.csv')
+        affix = f'{catchment_id}/{ts_id}/{date}.csv'
+        uri = f's3://{self.__s3_parameters.internal}/{self.__s3_parameters.path_internal_data}series/{affix}'
 
-        return self.__update(uri=uri, frame=frame)
+        return self.__update(uri=uri, frame=frame, affix=affix.replace('/', os.sep))
