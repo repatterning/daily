08c9ea189fdafad72c3a70763981c99b0d9ea604
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-21 02:59:34 +0100

    Update src/data/gauges.py

--- src/data/gauges.py
+++ src/data/gauges.py
@@ -9,6 +9,7 @@ import pandas as pd
 import src.elements.s3_parameters as s3p
 import src.elements.service as sr
 import src.s3.keys
+import src.data.filtering
 
 
 class Gauges:
@@ -16,19 +17,36 @@ class Gauges:
     Retrieves the catchment & time series codes of the gauges in focus.
     """
 
-    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters, attributes: dict):
         """
 
         :param service: A suite of services for interacting with Amazon Web Services.
         :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
                               name, buckets, etc.
+        :param attributes: A set of data acquisition attributes.
         """
 
         self.__service = service
         self.__s3_parameters = s3_parameters
+        self.__attributes = attributes
 
         # Instances
         self.__objects = src.s3.keys.Keys(service=self.__service, bucket_name=self.__s3_parameters.internal)
+        self.__filtering = src.data.filtering.Filtering()
+
+    def __get_codes(self, data: pd.DataFrame) -> dict:
+        """
+
+        :param data:
+        :return:
+        """
+
+        data = data if len(self.__attributes.get('excerpt')) == 0 \
+            else self.__filtering.exc(data=data, attributes=self.__attributes)
+        instances: pd.DataFrame = data.copy()[['catchment_id', 'ts_id']]
+        codes: dict = instances.set_index(keys='ts_id', drop=True).to_dict(orient='dict')['catchment_id']
+
+        return codes
 
     @dask.delayed
     def __get_section(self, listing: str) -> pd.DataFrame:
@@ -51,7 +69,7 @@ class Gauges:
 
         return frame
 
-    def exc(self) -> pd.DataFrame:
+    def exc(self) -> dict:
         """
 
         :return:
@@ -64,9 +82,9 @@ class Gauges:
             frame = self.__get_section(listing=listing)
             computations.append(frame)
         frames = dask.compute(computations, scheduler='threads')[0]
-        codes = pd.concat(frames, ignore_index=True, axis=0)
+        data = pd.concat(frames, ignore_index=True, axis=0)
 
-        codes['catchment_id'] = codes['catchment_id'].astype(dtype=np.int64)
-        codes['ts_id'] = codes['ts_id'].astype(dtype=np.int64)
+        data['catchment_id'] = data['catchment_id'].astype(dtype=np.int64)
+        data['ts_id'] = data['ts_id'].astype(dtype=np.int64)
 
-        return codes
+        return self.__get_codes(data=data)
